3a181e374bb4897179f7dc1f20b4c77b49a8cfe4
ROG <ROG@211.54.64.224>
2022-10-01 01:59:52 +0900

    DB 연결 끊음

--- src/main/java/com/mpark/MparkApplication.java
+++ src/main/java/com/mpark/MparkApplication.java
@@ -2,9 +2,10 @@ package com.mpark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @PropertySource("classpath:/config.properties")
 public class MparkApplication {
 
